1ca28ddd18d1beb9117b1f5faa8da545627ef8a6
klajdioshafi <klajdioshafi99@gmail.com>
2022-05-02 12:29:49 +0200

    2.0.1 - Design Fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FeatureComponent } from './feature/feature.component';
+import { ServiceComponent } from './service/service.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeatureComponent,
+    ServiceComponent,
+    TestimonialComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
